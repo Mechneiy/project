55388bceb7bfbc4d5e211fbbce8851076b09c378
Vital <mechenistalker@gmail.com>
2022-08-30 23:23:25 +0300

    html and js work

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberofFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMoviesDB = {
+  count: numberofFilms,
+  movies: {},
+  actor: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из последних просмотреных фильмов?", "");
+const b = prompt("на сколько оцените его?", "");
+const c = prompt("Один из последних просмотреных фильмов", "");
+const d = prompt("на сколько оцените его?", "");
+
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+console.log(personalMoviesDB);
