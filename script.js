f40a4bbe8314d2daf26061affc9ff6059339786b
Vital <mechenistalker@gmail.com>
2022-09-04 20:50:00 +0300

    js lesson

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-"use strict";
+// "use strict";
 
-const numberofFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const numberofFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMoviesDB = {
-  count: numberofFilms,
-  movies: {},
-  actor: {},
-  genres: [],
-  privat: false,
-};
+// const personalMoviesDB = {
+//   count: numberofFilms,
+//   movies: {},
+//   actor: {},
+//   genres: [],
+//   privat: false,
+// };
 
-const a = prompt("Один из последних просмотреных фильмов?", "");
-const b = prompt("на сколько оцените его?", "");
-const c = prompt("Один из последних просмотреных фильмов", "");
-const d = prompt("на сколько оцените его?", "");
+// const a = prompt("Один из последних просмотреных фильмов?", "");
+// const b = prompt("на сколько оцените его?", "");
+// const c = prompt("Один из последних просмотреных фильмов", "");
+// const d = prompt("на сколько оцените его?", "");
 
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
+// personalMoviesDB.movies[a] = b;
+// personalMoviesDB.movies[c] = d;
 
-console.log(personalMoviesDB);
+// console.log(personalMoviesDB);
